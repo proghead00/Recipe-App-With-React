76b13ee68983bf8446405c9414facc46d75767ce
unknown <susnatadj.rocks@gmail.com>
2020-08-28 15:16:26 +0530

    Updates

---
+++ precache-manifest.5d33e14b1e752d4e617210631ed2828f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ad21596e2bae0cf18563e22239e354",
+    "url": "/Recipe-App-With-React/index.html"
+  },
+  {
+    "revision": "32fc225a2dcfcb7b57dc",
+    "url": "/Recipe-App-With-React/static/css/main.25927b09.chunk.css"
+  },
+  {
+    "revision": "92756991f722857ec6ca",
+    "url": "/Recipe-App-With-React/static/js/2.f3f70430.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-App-With-React/static/js/2.f3f70430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32fc225a2dcfcb7b57dc",
+    "url": "/Recipe-App-With-React/static/js/main.ff74d2b6.chunk.js"
+  },
+  {
+    "revision": "1c2d5588593822a75bf1",
+    "url": "/Recipe-App-With-React/static/js/runtime-main.96a6742d.js"
+  }
+]);
